e17f0bb84e9dfd2340962fa651288e373e4f6b4b
traum <75057422+MREOY@users.noreply.github.com>
2024-04-30 14:00:34 +0900

    Update Dungeon.c

--- Dungeon.c
+++ Dungeon.c
@@ -7,12 +7,17 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define APPLE_APPEAR_PER 30
+#define APPLE_APPEAR_PER 40
+#define ENEMY_APPEAR_PER 80
+
 #define IMAGE_SIZE 100
 #define EXE_SIZE 500
 
+#define APPLE_HEAL 3
+#define UPSET_STOM 50
+
 //file
-char* readFile(char* fileName);
+char* readFile(char* fileName, char* path);
 FILE* createFile(char* fileName, char* path);
 FILE* getFile(char* fileName, char* path);
 void removeFile(char* fileName, char* path);
@@ -24,24 +29,35 @@ int checkFileNameOnRoot(char* name);
 
 //content
 int createDungeon(void);
-void createLayer(int layer,char* folderName, char* path);
-void createItem(int layer,char* folderName, char* path);
-void createDoor(int layer,char* folderName, char* path);
-void createApple(int layer,char* folderName, char* path);
+void createLayer(uint8_t layer,char* folderName, char* path);
+void createItem(uint8_t layer,char* folderName, char* path);
+void createDoor(uint8_t layer,char* folderName, char* path);
+void createApple(uint8_t layer,char* folderName, char* path);
+void createSlime(uint8_t layer,char* folderName, char* path);
 
 //utill
 int atoi_ten(char* str);
-int randomInt(int size);
+int randomInt(uint8_t size);
+
+void playerHeal(uint8_t heal);
+void playerDamage(uint8_t dmg);
+
+uint8_t getPlayerHP(void);
+uint8_t getPlayerDMG(void);
+void setPlayerHP(uint8_t hp);
+void setPlayerDMG(uint8_t dmg);
 
 void userAct(char *input);
 
 void actEat(void);
 void actEnter(void);
-
+void actState(void);
+void actEnemy(void);
 
 void init(void);
 
 char* APPLE_IMAGE[IMAGE_SIZE][IMAGE_SIZE];
+char* SLIME_IMAGE[IMAGE_SIZE][IMAGE_SIZE];
 char* DOOR_IMAGE[IMAGE_SIZE][IMAGE_SIZE];
 
 FILE *buffImage;
@@ -61,7 +77,7 @@ int main(void){
 
 	init();
 	if(checkFileNameOnRoot("Dungeon.txt")){
-        str = readFile("Dungeon.txt");
+        str = readFile("Dungeon.txt", ".");
         dungeonCreate++;
 
         deep = atoi_ten(str);
@@ -71,7 +87,7 @@ int main(void){
 
     if(dungeonCreate == 2) {
         printf("던전이 생성되었습니다!\n");
-        printf("eat - 음식을 먹는다\nquit - 종료\nenter - 문 열기\n\n");
+        printf("eat - 음식을 먹는다\nquit - 종료\nenter - 문 열기\nstate - 현재 상태\nenemy - 적 확인\n\n");
         gameRunning = 1;
     }
     else printf("던전이 생성되지 못했습니다!\n");
@@ -97,21 +113,40 @@ void init(void){
 
     getImage("apple.jpg", APPLE_IMAGE);
     getImage("door.jpg", DOOR_IMAGE);
+    getImage("slime.jpg", SLIME_IMAGE);
 }
 
 void userAct(char *input){
     if(strcmp(userInput, "eat") == 0) actEat();
     else if(strcmp(userInput, "enter") == 0) actEnter();
+    else if(strcmp(userInput, "state") == 0) actState();
+    else if(strcmp(userInput, "enemy") == 0) actEnemy();
 }
 
 void actEat(void){
+    char str[256];
+    int ran = (rand() % 100);
+
     printf("먹을 음식을 입력해주세요(아이템의 이름을 적어주세요)\n-");
     scanf("%s", userInput);
     if(checkFileName(userInput, inventoryRoot)){
         removeFile(userInput, inventoryRoot);
-        //switch
 
-        printf("음식을 먹었습니다.\n\n");
+        if(strcmp(str, "apple.jpg") == 0){
+             printf("사과를 먹었다!\n");
+            playerHeal(APPLE_HEAL);
+        }
+        else{
+            if(ran <= UPSET_STOM) {
+                printf("이상할걸 먹었더니 배탈이 났다...\n");
+                playerDamage(1);
+            }
+            else {
+                printf("맛이 좋지 않다...\n");
+                playerHeal(1);
+            }
+        }
+        printf("현재 채력 : %d\n\n", getPlayerHP());
     }else printf("음식을 찾을 수 없습니다.\n\n");
 
 }
@@ -120,7 +155,7 @@ void actEnter(void){
     char path[256];
     char str[256];
 
-    printf("경로를 복사 붙여넣어 주세요\n-");
+    printf("경로를 복사 및 붙여넣어 주세요\n-");
     scanf("%s", path);
 
     strcpy(str, path);
@@ -141,6 +176,24 @@ void actEnter(void){
 
 }
 
+void actState(void){
+    printf("HP : %d, DMG : %d\n\n", getPlayerHP(), getPlayerHP());
+}
+
+void actEnemy(void){
+    char str[256];
+
+    printf("적의 이름을 적어주세요.\n(slime)\n-");
+    scanf("%s", str);
+
+    if(strcmp(str, "slime") == 0)
+        printf("state : HP - 10 , DMG - 2");
+    else
+        printf("알 수 없는 적입니다.");
+
+    printf("\n\n");
+}
+
 /////////////////////////////////
 
 int createDungeon(void){
@@ -154,8 +207,7 @@ int createDungeon(void){
     if(charact == NULL){
         charact = getFile("Player.txt", "Dungeon");
     }else {
-        fputs("HP:20\n", charact);
-        fputs("AT:5\n", charact);
+        fputs("HP 20  AT 5", charact);
     }
     fclose(charact);
 
@@ -168,7 +220,7 @@ int createDungeon(void){
 }
 
 
-void createLayer(int layer,char* folderName, char* path){
+void createLayer(uint8_t layer,char* folderName, char* path){
     char buff[256];
     sprintf(buff, "%s\\%s", path, folderName);
 
@@ -176,41 +228,91 @@ void createLayer(int layer,char* folderName, char* path){
     createItem(layer, folderName, path);
 }
 
-void createItem(int layer,char* folderName, char* path){
+void createItem(uint8_t layer,char* folderName, char* path){
     createDoor(layer, folderName, path);
 	createApple(layer, folderName, path);
-
+    createSlime(layer, folderName, path);
 }
 
-void createDoor(int layer,char* folderName, char* path){
-	char buffName[50];
+void createDoor(uint8_t layer,char* folderName, char* path){
+	char buffName[256];
 	char buff[256];
-	int a;
+	uint8_t ran;
 
     sprintf(buff, "%s\\%s", path, folderName);
 
-	a = (rand() % 3) + 1;
+    if(layer <= deep){
+        ran = (rand() % 2);
 
-	for(i = 0; i < a; i++){
-        sprintf(buffName, "door(%d).jpg", (i + 1));
-        createImage(buffName, DOOR_IMAGE, buff);
-	}
+        if(ran == 0){
+            ran = (rand() % 3) + 1;
+
+            for(i = 0; i < ran; i++){
+            sprintf(buffName, "door(%d).jpg", (i + 1));
+            createImage(buffName, DOOR_IMAGE, buff);
+            }
+        }
+        else{
+            createImage("door.jpg", DOOR_IMAGE, buff);
+        }
+
+    }
 }
 
-void createApple(int layer,char* folderName, char* path){
+void createApple(uint8_t layer,char* folderName, char* path){
+    char buffName[256];
     char buff[256];
+    uint8_t ran;
+
     sprintf(buff, "%s\\%s", path, folderName);
 
+    if(layer <= deep){
+        ran = (rand() % 100);
 
-    if((rand() % 100) < APPLE_APPEAR_PER){
-        createImage("apple.jpg", APPLE_IMAGE, buff);
+        if(ran < APPLE_APPEAR_PER){
+            ran = (rand() % 2) + 1;
+
+            for(i = 0; i < ran; i++){
+            sprintf(buffName, "apple(%d).jpg", (i + 1));
+            createImage(buffName, APPLE_IMAGE, buff);
+            }
+        }
+        else{
+            createImage("apple.jpg", APPLE_IMAGE, buff);
+        }
+
+    }
+}
+
+void createSlime(uint8_t layer,char* folderName, char* path){
+    char buffName[256];
+    char buff[256];
+    uint8_t ran;
+
+    sprintf(buff, "%s\\%s", path, folderName);
+
+    if(layer <= deep){
+        ran = (rand() % 100);
+
+        if(ran < ENEMY_APPEAR_PER){
+            ran = (rand() % 2) + 1;
+
+            for(i = 0; i < ran; i++){
+            sprintf(buffName, "slime(%d).jpg", (i + 1));
+            createImage(buffName, SLIME_IMAGE, buff);
+            }
+        }
+        else{
+            createImage("slime.jpg", SLIME_IMAGE, buff);
+        }
+
     }
 }
 
 ////////////////////////////////
 
-int randomInt(int size){
-	int random = 0;
+int randomInt(uint8_t size){
+	uint8_t random = 0;
 	random = rand() % size;
 	return random;
 }
@@ -225,6 +327,70 @@ int atoi_ten(char* str){
 	turn += atoi(&str[0]);
 
 	return turn;
+}
+
+uint8_t getPlayerHP(void){
+    char *buff = malloc (sizeof (char) * 256);
+    uint8_t nowHP;
+
+    buff = readFile("Player.txt", "Dungeon");
+
+    sscanf(buff, "%*s %d", &nowHP);
+    return nowHP;
+}
+
+uint8_t getPlayerDMG(void){
+    char *buff = malloc (sizeof (char) * 256);
+    uint8_t dmg;
+
+    buff = readFile("Player.txt", "Dungeon");
+
+    sscanf(buff, "%*s %*d  %*s %d", &dmg);
+    return dmg;
+}
+
+void setPlayerHP(uint8_t hp){
+    FILE* charact = NULL;
+    char *str = malloc (sizeof (char) * 256);
+
+    charact = getFile("Player.txt", "Dungeon");
+    if(charact != NULL){
+        sprintf(str, "HP %d  AT %d", hp, getPlayerDMG());
+        fputs(str, charact);
+    }
+    fclose(charact);
+}
+
+void setPlayerDMG(uint8_t dmg){
+    FILE* charact = NULL;
+    char *str = malloc (sizeof (char) * 256);
+
+    charact = getFile("Player.txt", "Dungeon");
+    if(charact != NULL){
+        sprintf(str, "HP %d  AT %d", getPlayerHP(), dmg);
+        fputs(str, charact);
+    }
+    fclose(charact);
+}
+
+void playerHeal(uint8_t heal){
+    uint8_t nowHP = getPlayerHP();
+
+    if(nowHP + heal >= 99){
+        setPlayerHP(99);
+    }else{
+        setPlayerHP(nowHP + heal);
+    }
+}
+
+void playerDamage(uint8_t dmg){
+    uint8_t nowHP = getPlayerHP();
+
+    if(nowHP - dmg <= 0){
+        setPlayerHP(0);
+    }else{
+        setPlayerHP(nowHP - dmg);
+    }
 }
 
 ///////////////////////////////////
@@ -266,16 +432,18 @@ FILE* createFile(char* fileName, char* path){
 	return NULL;
 }
 
-char* readFile(char* fileName){
+char* readFile(char* fileName, char* path){
 	FILE* file;
+	char buff[100] = {0,};
 	char *str = malloc (sizeof (char) * 1024);
 
-	file = fopen(fileName, "r");
-	if(file == NULL) return 0;
-	else{
-		fgets(str, 1024, file);
-		fclose(file);
-		return str;
+	if(checkFileName(fileName, path)){
+        sprintf(buff, "%s\\%s", path, fileName);
+        file = fopen(buff, "r");
+
+        fgets(str, 1024, file);
+        fclose(file);
+        return str;
 	}
 }
 
